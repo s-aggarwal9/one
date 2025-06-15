067c227d9e1c3da7131127578ae85df03de91db6
Shourya <aggarwalshourya9@proton.me>
2025-06-15 20:46:46 +0530

    2 car

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { ProductType } from "@/sanity.types";
+import { AnimatePresence, motion } from "framer-motion";
 
-const ProductGrid = () => {
-  return <div>ProductGrid</div>;
+const ProductGrid = ({ products }: { products: ProductType[] }) => {
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <AnimatePresence key={product._id}>
+            <motion.div
+              layout
+              initial={{ opacity: 0.2 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="flex justify center"
+            >
+              {/* <ProductThumb key={product._id} product={product}/> */}
+            </motion.div>
+          </AnimatePresence>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProductGrid;
