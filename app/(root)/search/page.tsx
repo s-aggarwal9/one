a7fcfa9234c8d5a850d5fca422cdc21488fd1e9e
Shourya <aggarwalshourya9@proton.me>
2025-06-17 11:28:32 +0530

    rr 5

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const page = async ({ searchParams }: { searchParams: { query: string } }) => {
   const { query } = await searchParams;
+  const products = await searchProductsByName(query);
   return <div>page {query}</div>;
 };
 
