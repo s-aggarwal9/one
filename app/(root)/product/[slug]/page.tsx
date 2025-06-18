7761880aa7bb73c7c793ecd4325fb14adad903d1
Shourya <aggarwalshourya9@proton.me>
2025-06-18 12:46:38 +0530

    rr5

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,9 +1,44 @@
+import { imageUrl } from "@/lib/imageUrl";
+import getProductBySlug from "@/sanity/lib/products/getProductBySlug";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 import React from "react";
 
 const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
   const product = await getProductBySlug(slug);
-  return <div>page</div>;
+
+  if (!product) {
+    return notFound();
+  }
+
+  const isOutOfStock = product.stock != null && product.stock <= 0;
+  return (
+    <div>
+      <div>
+        <div>
+          {product.image && (
+            <Image
+              src={imageUrl(product.image).url()}
+              alt={product.name || "Product image"}
+              width={20}
+              height={20}
+            />
+          )}
+          {isOutOfStock && (
+            <div>
+              <span>Out of Stock</span>
+            </div>
+          )}
+        </div>
+        <div>
+          <h1>{product.name}</h1>
+          <div>{product.price?.toFixed(2)}</div>
+          {product.description && <p>{product.description}</p>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
