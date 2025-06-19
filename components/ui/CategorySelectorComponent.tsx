85689af482dd9e4c812be386f07294443059483a
Shourya <aggarwalshourya9@proton.me>
2025-06-19 16:51:54 +0530

    rr 4 categorySelector.tsx

--- components/ui/CategorySelectorComponent.tsx
+++ components/ui/CategorySelectorComponent.tsx
@@ -1,11 +1,28 @@
-import { Category } from "@/sanity.types";
-import React from "react";
+"use client";
 
+import { ChevronsUpDown, Check } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Category } from "@/sanity.types";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { Button } from "./button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+  CommandInput,
+} from "./command";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 interface CategorySelectorProps {
   categories: Category[];
 }
 
 const CategorySelectorComponent = ({ categories }: CategorySelectorProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState<string>("");
+  const router = useRouter();
   return <div>CategorySelectorComponent</div>;
 };
 
