4c7ee23854284b4b264cccc5a25b82258d68c20c
Shourya <aggarwalshourya9@proton.me>
2025-06-20 13:49:52 +0530

    rr4 removeItem, clearBasket

--- app/(root)/store.ts
+++ app/(root)/store.ts
@@ -39,6 +39,20 @@ const useBasketStore = create<BasketState>()(
             return { items: [...state.items, { product, quantity: 1 }] };
           }
         }),
+      removeItem: (prductId) =>
+        set((state) => ({
+          items: state.items.reduce((acc, item) => {
+            if (item.product._id === prductId) {
+              if (item.quantity > 1) {
+                acc.push({ ...item, quantity: item.quantity - 1 });
+              }
+            } else {
+              acc.push(item);
+            }
+            return acc;
+          }, [] as BasketItem[]),
+        })),
+      clearBasket: () => set({ items: [] }),
     }),
     { name: "basket-store" }
   )
