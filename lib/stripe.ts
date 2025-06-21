72ff48a73b96133f630dd3b7e4d3cbfe12e57ae9
Shourya <aggarwalshourya9@proton.me>
2025-06-21 12:22:00 +0530

    rr5 new stripe

---
+++ lib/stripe.ts
@@ -0,0 +1,8 @@
+import { apiVersion } from "@/sanity/env";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-05-28.basil",
+});
+
+export default stripe;
