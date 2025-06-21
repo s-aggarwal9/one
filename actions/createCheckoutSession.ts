99bf342d98a6f250ab3d54bf66c0454d95e22999
Shourya <aggarwalshourya9@proton.me>
2025-06-21 12:58:00 +0530

    rr9 check stripe stripe checkout session cont

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -17,7 +17,7 @@ export type GroupedBasketItem = {
 
 export async function createCheckoutSession(
   items: GroupedBasketItem[],
-  metedata: Metadata
+  metadata: Metadata
 ) {
   try {
     // check if product without price
@@ -28,7 +28,7 @@ export async function createCheckoutSession(
 
     // search for existing customer by email
     const customers = await stripe.customers.list({
-      email: metedata.customerEmail,
+      email: metadata.customerEmail,
       limit: 1,
     });
 
@@ -41,7 +41,12 @@ export async function createCheckoutSession(
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
       customer_creation: customerId ? undefined : "always",
-      customer_email: !customerId ? metedata.customerEmail : undefined,
+      customer_email: !customerId ? metadata.customerEmail : undefined,
+      metadata,
+      mode: "payment",
+      allow_promotion_codes: true,
+      success_url: "",
+      cancel_url: "",
     });
   } catch (error) {
     console.error("Error creating checkout session: ", error);
