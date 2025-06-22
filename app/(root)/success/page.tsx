2c52cced9a6c03f8d8a4a11fb37b2c1e2bd8eef8
Shourya <aggarwalshourya9@proton.me>
2025-06-22 11:42:00 +0530

    rr4 Success Page

--- app/(root)/success/page.tsx
+++ app/(root)/success/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import useBasketStore from "../store";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const SuccessPage = () => {
+  const searchParams = useSearchParams();
+  const orderNumber = searchParams.get("orderNumber");
+  const clearBasket = useBasketStore((state) => state.clearBasket);
+
+  useEffect(() => {
+    if (orderNumber) {
+      clearBasket();
+    }
+  }, [orderNumber, clearBasket]);
+  return (
+    <div>
+      <div>Success</div>
+      <div>tick</div>
+      <h1>Thank You for order</h1>
+      <span>{orderNumber}</span>
+      <div>
+        <Button asChild>
+          <Link href="/orders"> View Order Details</Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/"> Continue Shopping</Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default SuccessPage;
