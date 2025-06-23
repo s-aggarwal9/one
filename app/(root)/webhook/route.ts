68e4c8854438f0e11dc9c7703e870290c1fd0164
Shourya <aggarwalshourya9@proton.me>
2025-06-23 10:13:00 +0530

    rr2 Silver Jubilee 25L

--- app/(root)/webhook/route.ts
+++ app/(root)/webhook/route.ts
@@ -5,5 +5,31 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  const body = await req.text();
+  const headersList = await headers();
+  const sig = headersList.get("stripe-signature");
+
+  if (!sig) {
+    return NextResponse.json({ error: "No signature" }, { status: 400 });
+  }
+
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  if (!webhookSecret) {
+    return NextResponse.json(
+      { error: "webhook secret is required" },
+      { status: 400 }
+    );
+  }
+
+  let event: Stripe.Event;
+
+  try {
+  } catch (error) {
+    console.error("Webhook signature verification failed:", error);
+    return NextResponse.json(
+      { error: `webhook error: ${error}` },
+      { status: 400 }
+    );
+  }
 }
