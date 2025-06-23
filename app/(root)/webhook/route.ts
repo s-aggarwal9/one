63305cd9c2faef16b327815b505be6aef784ff80
Shourya <aggarwalshourya9@proton.me>
2025-06-23 10:02:00 +0530

    rr1 Kittu Bday

---
+++ app/(root)/webhook/route.ts
@@ -0,0 +1,9 @@
+import stripe from "@/lib/stripe";
+import { backendClient } from "@/sanity/lib/backendClient";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+}
