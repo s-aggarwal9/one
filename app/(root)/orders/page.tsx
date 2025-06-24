b23e9fb5382ca8f47b5c2f30e13561948690ae97
Shourya <aggarwalshourya9@proton.me>
2025-06-24 18:23:55 +0530

    RR2 Silver Jubilee 25L

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,3 +1,4 @@
+import { getMyOrders } from "@/sanity/lib/orders/getMyOrders";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -11,7 +12,26 @@ const Orders = async () => {
 
   const orders = await getMyOrders(userId);
 
-  return <div>Orders</div>;
+  return (
+    <div>
+      <div>
+        <h1>Orders</h1>
+        {orders.length === 0 ? (
+          <div>
+            <p>You have not placed any orders yet</p>
+          </div>
+        ) : (
+          <div>
+            {orders.map((order) => (
+              <div key={order.orderNumber}>
+                <h2>Order {order.orderNumber}</h2>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
